bb09b3013269cbd3cc1a290226af6033d3dc67e7
NikNitro <adrimlg92@gmail.com>
2020-04-11 14:14:31 -0400

    Update payload.php

--- testing/payload.php
+++ testing/payload.php
@@ -1,4 +1,4 @@
 <?php echo '<p>Hello World</p>'; ?> 
-<?php echo '<script>alert("XSS");</script>'; ?> 
 <?php echo 'scandir(".");'; ?> 
 <?php scandir('.'); ?> 
+<?php echo '<script>alert("XSS");</script>'; ?> 
